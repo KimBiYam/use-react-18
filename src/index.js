15fa007caae2c857b3dbbb4dcf017b49974459fa
ChangHyunKim <hot9998@naver.com>
2022-01-19 11:51:49 +0900

    Apply create root function to root element

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
 
-ReactDOM.render(<App />, container);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
